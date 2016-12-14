fec79e07142ee753bfc76d9b843bc242ab2de0a3
yanming <yanming-sohu@sohu.com>
2016-12-14 20:04:51 +0800

    集群参数变更 Signed-off-by: yanming <yanming-sohu@sohu.com>

--- test/demo.js
+++ test/demo.js
@@ -3,7 +3,8 @@ var express = require('express');
 var logger  = require('logger-lib');
 
 
-demo2();
+// demo2();
+demo1();
 
 
 // ---------------------------------------------------------
@@ -14,13 +15,15 @@ function whenLoad(app_pool) {
   var ex_app = require('./express-template.js');
   var nv_app = require('./native-template.js');
   //var tk_app = require('./thinkjs-template.js');
-  
+
   ex_app(app_pool);
   nv_app(app_pool);
   //tk_app(app_pool);
   app_pool.addApp(reload(app_pool.reload), '/reload');
 
-  logger.info("routes: ", app_pool.route_list());
+  app_pool.on('afterFirstLoad', function() {
+    logger.info("routes: ", app_pool.route_list());
+  });
 };
 
 
@@ -37,6 +40,9 @@ function demo1() {
   var conf = {};
   // 在 create_http_mix_server() 中, 该方法被回调, 进行初始化
   conf.whenLoad = whenLoad;
+  // conf.cluster = 0.5;
+  // conf.cluster = -22;
+  // conf.cluster = 2;
   mixer.create_http_mix_server(conf);
 }
 
