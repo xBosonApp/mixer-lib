a71e83435d6c1e0337e18a990f968bb166408fcb
yanming <yanming-sohu@sohu.com>
2019-03-22 10:03:03 +0800

    Signed-off-by: yanming <yanming-sohu@sohu.com>

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var ex = module.exports;
 var util = ex.util = require('./lib/util.js');
-	
+
 
 util.bind('route.js', ex);
 util.bind('server-mixer.js', ex);
 util.bind('compatible.js', ex);
+util.bind('cluster.js', ex);
 
-	
+
 util.dynamicBind('net');
 util.dynamicBind('data');
 util.dynamicBind('mid');
